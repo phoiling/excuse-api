21991a68400ece49547679eb7588b01785bea2f7
phoiling <daleleyland@icloud.com>
2026-05-12 06:41:56 +0000

    added coming soon to aip requests

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI(title = "Excuse Generator")
 @app.get("/")
 def root():
     return{"name": "Excuse Generator", "try": "/excuse?situation=I forgot my homework"}
+
+@app.get("/excuse")
+async def get_excuse(situation: str):
+    return {"situation": situation, "excuse": "coming soon!"}
