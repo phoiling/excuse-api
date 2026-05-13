5ee0e9526011d9e13cd3e85d4cb7d6b908885e83
phoiling <daleleyland@icloud.com>
2026-05-13 08:36:53 +0000

    removed testinng data

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 import httpx
 import os
  
-load_dotenv()
+load_dotenv("api_key.env")
 KEY = os.getenv("HACKCLUB_AI_KEY")
+print(f"Key loaded: {KEY}")
 
 app = FastAPI(title = "Excuse Generator")
 
@@ -16,16 +17,15 @@ def root():
 async def get_excuse(situation: str):
     async with httpx.AsyncClient() as client:
         response = await client.post(
-            "https://ai.hackclub.com/chat/completions",
+            "https://ai.hackclub.com/proxy/v1/chat/completions",
             headers={"Authorization": f"Bearer {KEY}"},
             json={
-                "model": "gpt-4o-mini",
+                "model": "google/gemma-4-31b-it:free",
                 "messages": [
                     {"role": "system", "content": "You generate funny, creative, slightly unhinged excuses. One sentence only. No quotes around your answer."},
                     {"role": "user", "content":  f"Give me an excuse for: {situation}"} ,
                 ],   
             }
-            
         )
         data = response.json()
         excuse = data["choices"][0]["message"]["content"]
